fdc3183b97319a9f30854e15338947f89adb7c9a
gustavovni08 <gustavovni08@gmail.com>
2023-12-01 10:57:20 -0300

    toWork

---
+++ src/components/LandingPageCardPrecos.vue
@@ -0,0 +1,186 @@
+<template>
+    <div class="card-container">
+        <div class="title-container">
+            <p>{{ title }}</p>
+        </div>
+        <div class="price-container">
+            <div class="einstein-price-label"
+            v-if="einsteinPrice-price > 0">
+                <span class="line"></span>
+                <p> R${{ einsteinPrice }}</p>
+            </div>
+
+            <div class="price-label">
+                <p> <strong>R${{ price }}</strong>/mensal</p>
+            </div>
+        </div>
+        <div class="info-container">
+                <div class="info" v-if=" einsteinPrice - price > 0">
+                    <span class="material-symbols-outlined">
+                    savings
+                    </span>
+                    <p>Economize {{ einsteinPrice - price }}R$</p>
+                </div>
+
+                <div class="info" v-if=" users > 0 ">
+                    <span class="material-symbols-outlined">
+                        group
+                    </span>
+                    <p> 
+                        A partir de {{ users }} pessoas
+                    </p>
+                </div>
+                
+
+
+
+        </div>
+        <div class="button-container">
+            Assine já
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        price:Number,
+        users:Number,
+        title:String,
+        einsteinPrice:Number,
+    }
+}
+</script>
+<style scoped lang="css">
+    .card-container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: #ffff;
+        border: solid  rgba(0, 0, 0, 0.2) 0.6px;
+
+        height: 435px;
+        width: 270px;
+        margin: 40px;
+
+        box-shadow: 2px 3px 4px rgba(0, 0, 0, 0.1);
+
+        border-radius: 12px;
+    }
+
+    .card-container:hover{
+        height: 455px;
+        width: 290px;
+    }
+
+    .title-container{
+        display: flex;
+        align-items: center;
+        height: 23%;
+
+    }
+
+    .title-container p {
+        font-weight: bold;
+        font-size: 26px;
+        text-align: center;
+        color:#082777;
+    }
+
+
+    .price-container{
+        display: flex;
+        flex-direction: column;
+
+
+        height: 20%;
+        position: relative;
+        top: -40px;
+    }
+
+    .einstein-price-label{
+
+        display: inline-block;
+        position: relative;
+        
+    }
+
+    .line{
+      display: flex;
+      justify-content: center;
+      position: absolute;
+      bottom: 50%;
+      left: 27px;
+      width: 70%;
+      height: 0.1px;
+      background-color: black; /* Cor da linha */
+      transform: translateY(50%);
+    }
+
+    .price-label{
+        display: flex;
+        position: relative;
+        top: -10px
+    }
+
+    .price-label strong{
+        font-size: 30px;
+        color:#082777;
+    }
+
+    .price-label p{
+        font-weight: bold;
+    }
+
+    .info-container{
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        height: 36%;
+
+        position: relative;
+        top: -20px;
+    }
+
+    .info{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 80%;
+        padding-left: 10px;
+    }
+
+    .info p{
+        max-width: 147.8px;
+        text-align: center;
+    }
+
+    .info span{
+        color:#082777;
+    }
+
+    .button-container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #082777;
+        
+        font-weight: bold;
+        color: #fff;
+
+        width: 170px;
+        height: 44px;
+        border-radius: 8px;
+
+        cursor: pointer;
+        box-shadow: 2px 3px 4px rgba(0, 0, 0, 0.1);
+    }
+
+    .button-container:hover{
+        background-color:#243d7b;
+    }
+
+
+
+
+
+</style>
---
+++ src/views/LandingPageHome.vue
@@ -0,0 +1,242 @@
+<template>
+    <div class="main-container">
+        <section class="home-section">
+
+            <div class="info-container">
+                <div class="h1-container">
+                    <p class="h1-text">Sua saúde <br>protegida por</p>
+                    <h1 class="h1-price">R$ 29,90</h1>
+                </div>
+               
+                <div class="button home-button"
+                @click="irParaPrecos">
+                    Assine já
+                </div>
+            </div>
+
+            <div class="img-container">
+                <img :src="require('@/assets/homeImg.png')" alt="homeImg">
+            </div>
+        </section>
+
+        <br>
+        
+        <section class="preços">
+            <div class="card-container"
+            v-for="(plano, index) in planos" :key="index"
+            >
+                <LandingPageCardPrecos
+                :title="plano.title"
+                :users="plano.users"
+                :einsteinPrice="plano.einsteinPrice"
+                :price="plano.price"
+                />
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+
+import LandingPageCardPrecos from '@/components/LandingPageCardPrecos.vue';
+
+export default {
+    data(){
+        return{
+            planos:[
+                {
+                    einsteinPrice: 129.90,
+                    title: 'Individual',
+                    price: 39.90,
+                    users: 0,
+                },
+                {
+                    einsteinPrice: 99.90,
+                    title: 'Familiar',
+                    price: 29.90,
+                    users: 2,
+                },
+                // {
+                //     einsteinPrice: 0,
+                //     title: 'Empresarial',
+                //     price: 19.90,
+                //     users: 10,
+                // },
+            ]
+        }
+    },
+
+    methods:{
+        irParaPrecos(){
+            const precos = document.querySelector('.preços')
+            precos.scrollIntoView({behavior:'smooth'})
+        }
+    },
+
+
+    components:{LandingPageCardPrecos}
+}
+</script>
+
+
+<style scoped lang="css">
+
+    .main-container{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+    }
+
+    .home-section{
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        height: 80vh;
+        margin-top: 60px;
+    }
+    .info-container{
+        display: flex;
+        flex-direction: column;
+    }
+    .h1-container{
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+
+    }
+    .h1-text{
+        color:#082777;
+        font-size: 50px;
+        text-align: left;
+        font-weight: bold;
+
+        animation: fadeInRight 1s ease-out forwards;
+    }
+    .h1-price{
+        position: relative;
+        top: -60px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 48px;
+        padding: 21px;
+        padding-bottom: 10px;
+        padding-top: 10px;
+        padding-right: 35px;
+        background-color: #082777;
+        color: #ffff;
+        border-radius: 12px;
+        animation: fadeInRight 1s ease-out forwards;
+        box-shadow: 8px 4px 8px rgba(0, 0, 0, 0.1);
+    }
+    @keyframes fadeInRight {
+    /* No início da animação (0%), a opacidade é 0 e a posição X é 100% */
+    0% {
+        opacity: 0;
+        transform: translateX(100%);
+    }
+
+    /* No final da animação (100%), a opacidade é 1 e a posição X é 0 */
+    100% {
+        opacity: 1;
+        transform: translateX(0);
+    }
+    }
+    .button{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        background-color: #FBBB3F;
+        border-radius: 12px;
+        font-weight: bold;
+        color: #ffff;
+
+        box-shadow: 8px 4px 8px rgba(0, 0, 0, 0.1);
+        cursor: pointer;
+
+
+    }
+    .home-button{
+        animation: fadeIn 1.6s ease-out forwards;
+
+        width: 80%;
+        padding-left: 35px;
+        padding-right: 35px;
+        padding-top: 20px;
+        padding-bottom: 20px;
+
+        font-size: 50px;
+        position: relative;
+        top: -30px;
+    }
+    @keyframes fadeIn {
+    /* No início da animação (0%), a opacidade é 0 */
+    0% {
+        opacity: 0;
+    }
+
+    /* No final da animação (100%), a opacidade é 1 */
+    100% {
+        opacity: 1;
+    }
+    }
+    .button:hover{
+        background-color: #feca62;
+    }
+    .img-container{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        animation: fadeIn 1.8s ease-out forwards;
+       
+    }
+    .img-container img{
+        position: relative;
+        top: 47px;
+/* 
+        /* max-width: 50vw; */
+        max-height: 80vh;
+    }
+
+
+    @media  screen and (max-width: 800px) {
+        .home-section{
+            flex-direction: column;
+            margin-top: 200px;
+        }
+
+        .h1-container{
+            align-items: center;
+        }
+
+        .h1-text{
+            text-align: center;
+        }
+
+        .img-container img{
+        position: inline;
+        top: 47px;
+/* 
+        /* max-width: 50vw; */
+        max-width: 80%;
+        }
+
+        .preços{
+            flex-direction: column;
+            margin-top: 680px;
+        }
+    }
+
+    .preços{
+        
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 80vh;
+    }
+
+
+</style>
