1e10fa71e9f828d8628860aee725c2c2d5d419a8
gustavovni08 <gustavovni08@gmail.com>
2023-12-19 12:10:26 -0300

    deply?

--- dist/server.js
+++ dist/server.js
@@ -2,10 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const porta = process.env.PORT || 8080;
 
-// Servir arquivos estáticos a partir da pasta 'dist'
-app.use(express.static(path.join(__dirname, 'dist')));
+// Configurar a aplicação express e adicionar rotas, middlewares, etc.
 
 // Rota catch-all para servir o index.html
 app.get('*', (req, res) => {
@@ -13,6 +12,6 @@ app.get('*', (req, res) => {
 });
 
 // Iniciar o servidor
-app.listen(port, () => {
-  console.log(`Servidor está ouvindo na porta ${port}`);
+app.listen(porta, () => {
+  console.log(`Servidor está ouvindo na porta ${porta}`);
 });
