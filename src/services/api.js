1f2fa24429259110fd31cb7632c7009c0419ab00
gustavovni08 <gustavovni08@gmail.com>
2024-01-23 09:48:24 -0300

    a

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const api0 = null
+
 const api = axios.create({
-    baseURL: "https://hbcard.com.br/api"
+    baseURL: "https://hbcard.com.br/api",
 })
 
 export default api
