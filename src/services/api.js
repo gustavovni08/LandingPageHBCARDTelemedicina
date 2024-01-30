3b9ca7cff83243b6e987f5a858e98752e7faffb4
gustavovni08 <gustavovni08@gmail.com>
2024-01-30 15:28:07 -0300

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const url = "http://localhost:3000"
-// const url = "https://hbcard.com.br/api" 
+// const url = "http://localhost:3000"
+ const url = "https://hbcard.com.br/api" 
 
 const api = axios.create({
     baseURL: url,
